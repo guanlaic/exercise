574505f5463fbfa83b9a0e6405c4460d45e7ddf6
guanlaic <guanlaic@gmail.com>
2023-08-05 13:59:58 +0800

    first commit

---
+++ data.py
@@ -0,0 +1,34 @@
+'''
+python 資料型別
+
+
+num = 82 # 整數 int
+num2 = 88.9 # 浮點數 float
+
+
+
+布林值 bool
+
+True
+Flase
+
+bag = True # 布林值
+
+
+字串 str
+
+str = '這是字串'
+str2 = '嗨!早安 "' # 在字串加上字面上雙引號的方法
+str3 = '嗨! \n 早安" # 特殊字元 \n 換行
+
+
+取得資料型別
+
+語法 type(項目)
+
+print(type(9)) # <class 'int'>
+print(type(10.2)) # <class 'float'>
+print(type(True)) # <class 'bool'>
+
+
+'''
---
+++ pi.py
@@ -0,0 +1,8 @@
+ro = input("請輸入直徑")
+jfg = input("請輸入圓周率")
+
+gc = int(ro) * float(jfg)
+
+ck = "圓周長 :"
+
+print ("{:5s}{:.1f}".format(ck,gc))
